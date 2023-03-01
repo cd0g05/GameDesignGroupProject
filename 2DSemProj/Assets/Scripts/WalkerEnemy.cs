ef6bffe03b2eacfe3af56c2c6710ea0104ce8a24
BewareMe <124645717+BewareMe@users.noreply.github.com>
2023-03-01 14:52:19 -0800

    made basic walker functionality, edited sprite of basic walker, edited enemy testing scene and added wall tile map
    3/1/2023
    2:52 pm

--- 2DSemProj/Assets/Scripts/WalkerEnemy.cs
+++ 2DSemProj/Assets/Scripts/WalkerEnemy.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class WalkerEnemy : MonoBehaviour
 {
+    //declaring variables
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private bool onGround;
+    private Rigidbody2D walkerRb;
+    private Transform walkerTrans;
+    private BoxCollider2D walkerBc;
+    private bool walkingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        walkerRb = GetComponent<Rigidbody2D>();
+        walkerBc = GetComponent<BoxCollider2D>();
+        walkerTrans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (walkingRight)
+        {
+            walkerRb.velocity = new Vector2(speed, walkerRb.velocity.y);
+        }
+        else if (!walkingRight)
+        {
+            walkerRb.velocity = new Vector2(-speed, walkerRb.velocity.y);
+        }
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Walls"))
+        {
+            walkingRight = !walkingRight;
+            if(walkingRight)
+            {
+                //facing right
+                walkerTrans.localScale = new Vector3(-Mathf.Abs(walkerTrans.localScale.x), walkerTrans.localScale.y, walkerTrans.localScale.z);
+            } else if (!walkingRight)
+            {
+                //facing left
+                walkerTrans.localScale = new Vector3(Mathf.Abs(walkerTrans.localScale.x), walkerTrans.localScale.y, walkerTrans.localScale.z);
+            }
+        }
     }
 }
