63ee9be4653829c307b5c1836a95c7ca039dd825
BewareMe <124645717+BewareMe@users.noreply.github.com>
2023-03-06 14:01:26 -0800

    made hearts system outline
    3/6/2023
    2:01 pm

---
+++ 2DSemProj/Assets/Scripts/HealthController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public int playerHealth;
+
+    [SerializeField] private Image[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealth()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < playerHealth)
+            {
+                hearts[i].color = Color.red;
+            }
+            else
+            {
+                hearts[i].color = Color.black;
+            }
+        }
+    }
+}
