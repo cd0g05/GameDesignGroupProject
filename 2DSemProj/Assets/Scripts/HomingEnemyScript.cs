3d6e93b7eb211e7e501d261dc4c95d68f8dd0446
BewareMe <124645717+BewareMe@users.noreply.github.com>
2023-03-06 13:35:51 -0800

    made homing enemy script and edited the sprite of enemy2 projectile
    3/6/2023
    1:35 pm

---
+++ 2DSemProj/Assets/Scripts/HomingEnemyScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingEnemyScript : MonoBehaviour
+{
+    //declaring variables
+    [SerializeField] private float speed;
+    private Rigidbody2D homingRb;
+    private Transform homingTrans;
+    private BoxCollider2D homingBc;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        homingRb = GetComponent<Rigidbody2D>();
+        homingBc = GetComponent<BoxCollider2D>();
+        homingTrans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        homingRb.velocity = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y).normalized * speed;
+    }
+}
