9ed52ca35697a530123171f7592c74a5518b544e
davitho23 <tmdavies05@gmail.com>
2023-03-22 15:43:25 -0700

    Did shield
    3/22/23
    3:43 pm

---
+++ 2DSemProj/Assets/Scripts/PlayerShield.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShield : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float shieldCooldown;
+    [SerializeField] private float shieldTimer;
+    void Start()
+    {
+        shieldTimer = 5;
+        shieldCooldown = 5;
+        transform.parent = GameObject.Find("Player").transform;
+        transform.localPosition = new Vector2(0.55f, 0);
+        GameObject.Find("Shield").GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("Shield").GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shieldCooldown > 0)
+        {
+            shieldCooldown -= Time.deltaTime;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Y) && shieldCooldown <= 0)
+        {
+            GameObject.Find("Shield").GetComponent<SpriteRenderer>().enabled = true;
+            GameObject.Find("Shield").GetComponent<BoxCollider2D>().enabled = true;
+            shieldCooldown = 10;
+            GameObject.Find("HealthController").GetComponent<HealthController>().UseShield(shieldTimer);
+        }
+    }
+}
