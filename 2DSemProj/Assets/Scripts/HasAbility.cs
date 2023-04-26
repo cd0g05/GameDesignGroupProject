37d25f1dc2b5296e5889f745ff9eb233604d2bee
cd0g05 <carter.cripe@gmail.com>
2023-04-26 14:55:55 -0700

    Finished Inventory

--- 2DSemProj/Assets/Scripts/HasAbility.cs
+++ 2DSemProj/Assets/Scripts/HasAbility.cs
@@ -17,7 +17,7 @@ public class HasAbility : MonoBehaviour
     public bool hasAbility6 = false;
     public bool hasAbility7 = false;
     public bool hasAbility8 = false;
-    public bool hasAbility9 = false;
+    public bool hasAbility9 = false;     
     public bool hasAbility10 = false;
     public bool hasAbility11 = false;
     public bool hasAbility12 = false;
@@ -36,20 +36,27 @@ public class HasAbility : MonoBehaviour
         SetAbility3();
         SetAbility4();
         SetAbility5();
+        SetAbility6();
+        SetAbility7();
+        SetAbility8();
+        SetAbility9();
+        SetAbility10();
+        SetAbility11();
+        SetAbility12();
+
     }
     
     //Goes through inventory and checks if the item amount is one
 
     public void SetAbility1()
     {
-        Debug.Log(Inventory.Container[0].amount);
-        if (Inventory.Container[0].amount.Equals(1))
+        if (Inventory.Container[0].amount.Equals(1) && !hasAbility1)
         {
             hasAbility1 = true;
             Debug.Log("Set ability 1 to true");
 
         }
-        else
+        else if(Inventory.Container[0].amount.Equals(0) && hasAbility1)
         {
             hasAbility1 = false;
             Debug.Log("Set ability 1 to false");
@@ -59,12 +66,12 @@ public class HasAbility : MonoBehaviour
     }
     public void SetAbility2()
     {
-        if (Inventory.Container[1].amount.Equals(1))
+        if (Inventory.Container[1].amount.Equals(1) && !hasAbility2)
         {
             hasAbility2 = true;
             Debug.Log("Set ability 2 to true");
         }
-        else
+        else if(Inventory.Container[1].amount.Equals(0) && hasAbility2)
         {
             hasAbility2 = false;
             Debug.Log("Set ability 2 to false");
@@ -73,12 +80,12 @@ public class HasAbility : MonoBehaviour
     }
     public void SetAbility3()
     {
-        if (Inventory.Container[2].amount.Equals(1))
+        if (Inventory.Container[2].amount.Equals(1) && !hasAbility3)
         {
             hasAbility3 = true;
             Debug.Log("Set ability 3 to true");
         }
-        else
+        else if(Inventory.Container[2].amount.Equals(0) && hasAbility3)
         {
             hasAbility3 = false;
             Debug.Log("Set ability 3 to false");
@@ -88,12 +95,12 @@ public class HasAbility : MonoBehaviour
     }
     public void SetAbility4()
     {
-        if (Inventory.Container[3].amount.Equals(1))
+        if (Inventory.Container[3].amount.Equals(1) && !hasAbility4)
         {
             hasAbility4 = true;
             Debug.Log("Set ability 4 to true");
         }
-        else
+        else if(Inventory.Container[3].amount.Equals(0) && hasAbility4)
         {
             hasAbility4 = false;
             Debug.Log("Set ability 4 to false");
@@ -103,12 +110,12 @@ public class HasAbility : MonoBehaviour
     }
     public void SetAbility5()
     {
-        if (Inventory.Container[4].amount.Equals(1))
+        if (Inventory.Container[4].amount.Equals(1) && !hasAbility5)
         {
             hasAbility5 = true;
             Debug.Log("Set ability 5 to true");
         }
-        else
+        else if(Inventory.Container[4].amount.Equals(0) && hasAbility5)
         {
             hasAbility5 = false;
             Debug.Log("Set ability 5 to false");
@@ -116,7 +123,111 @@ public class HasAbility : MonoBehaviour
         }
         
     }
+    public void SetAbility6()
+    {
+        if (Inventory.Container[5].amount.Equals(1) && !hasAbility6)
+        {
+            hasAbility6 = true;
+            Debug.Log("Set ability 6 to true");
 
+        }
+        else if(Inventory.Container[5].amount.Equals(0) && hasAbility6)
+        {
+            hasAbility6 = false;
+            Debug.Log("Set ability 6 to false");
+
+        }
+
+    }
+    public void SetAbility7()
+    {
+        if (Inventory.Container[6].amount.Equals(1) && !hasAbility7)
+        {
+            hasAbility7 = true;
+            Debug.Log("Set ability 7 to true");
+        }
+        else if(Inventory.Container[6].amount.Equals(0) && hasAbility7)
+        {
+            hasAbility7 = false;
+            Debug.Log("Set ability 7 to false");
+
+        }
+    }
+    public void SetAbility8()
+    {
+        if (Inventory.Container[7].amount.Equals(1) && !hasAbility8)
+        {
+            hasAbility8 = true;
+            Debug.Log("Set ability 8 to true");
+        }
+        else if(Inventory.Container[7].amount.Equals(0) && hasAbility8)
+        {
+            hasAbility8 = false;
+            Debug.Log("Set ability 8 to false");
+
+        }
+        
+    }
+    public void SetAbility9()
+    {
+        if (Inventory.Container[8].amount.Equals(1) && !hasAbility9)
+        {
+            hasAbility9 = true;
+            Debug.Log("Set ability 9 to true");
+        }
+        else if(Inventory.Container[8].amount.Equals(0) && hasAbility9)
+        {
+            hasAbility9 = false;
+            Debug.Log("Set ability 9 to false");
+
+        }
+        
+    }
+    public void SetAbility10()
+    {
+        if (Inventory.Container[9].amount.Equals(1) && !hasAbility10)
+        {
+            hasAbility10 = true;
+            Debug.Log("Set ability 10 to true");
+        }
+        else if(Inventory.Container[9].amount.Equals(0) && hasAbility10)
+        {
+            hasAbility10 = false;
+            Debug.Log("Set ability 10 to false");
+
+        }
+        
+    }
+    public void SetAbility11()
+    {
+        if (Inventory.Container[10].amount.Equals(1) && !hasAbility11)
+        {
+            hasAbility11 = true;
+            Debug.Log("Set ability 11 to true");
+        }
+        else if(Inventory.Container[10].amount.Equals(0) && hasAbility11)
+        {
+            hasAbility11 = false;
+            Debug.Log("Set ability 11 to false");
+
+        }
+        
+    }
+    public void SetAbility12()
+    {
+        if (Inventory.Container[11].amount.Equals(1) && !hasAbility12)
+        {
+            hasAbility12 = true;
+            Debug.Log("Set ability 12 to true");
+        }
+        else if(Inventory.Container[11].amount.Equals(0) && hasAbility12)
+        {
+            hasAbility12 = false;
+            Debug.Log("Set ability 12 to false");
+
+        }
+        
+    }
 
 
 }
